5d1b9b3dae8ecab27f546ca38cb8759b78febd4c
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-04-14 17:30:16 +0000

    added middleware for protective routes

---
+++ middleware.ts
@@ -0,0 +1,55 @@
+import { withAuth } from "next-auth/middleware";
+
+import { NextResponse } from "next/server";
+
+export default withAuth(
+    async function middleware(req) {
+        const pathname = req.nextUrl.pathname;
+
+        const isAuth = req.cookies.get("refresh_token");
+
+        const sensitiveRoutes = [
+            "/",
+            "/dashboard",
+            "/dashboard/contact",
+            "/dashboard/sms",
+            "/dashboard/sms-logs",
+            "/dashboard/templates",
+        ];
+
+        const isAccessingSensitiveRoute = sensitiveRoutes.includes(pathname);
+
+        const sensitiveRoutesAfterLogin = [
+            "/login",
+            "/signup",
+            "/forget-password",
+            "/reset-password",
+        ];
+
+        const isAccessingSensitiveRoutesAfterLogin =
+            sensitiveRoutesAfterLogin.includes(pathname);
+
+        if (!isAuth && isAccessingSensitiveRoute) {
+            return NextResponse.redirect(new URL("/login", req.url));
+        }
+
+        // if (pathname == "/") {
+        //     return NextResponse.redirect(new URL("/", req.url));
+        // }
+
+        if (isAuth && isAccessingSensitiveRoutesAfterLogin) {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    },
+    {
+        callbacks: {
+            async authorized() {
+                return true;
+            },
+        },
+    }
+);
+
+export const config = {
+    matcher: ["/", "/login", "/signup", "/forget-password", "/reset-password"],
+};
