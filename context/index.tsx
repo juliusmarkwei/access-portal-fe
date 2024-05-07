1cf995b69f7f7fde4684fc66c6a1120f057460e4
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-05-07 22:21:26 +0000

    filterKeys stateful variable

--- context/index.tsx
+++ context/index.tsx
@@ -1,3 +1,4 @@
+// context.tsx
 import React, { createContext, useState, useContext } from "react";
 
 interface Keystype {
@@ -11,6 +12,10 @@ interface Keystype {
 interface AppContextType {
     keys: Keystype[] | undefined;
     setKeys: React.Dispatch<React.SetStateAction<Keystype[] | undefined>>;
+    filteredKeys: Keystype[] | undefined;
+    setFilteredKeys: React.Dispatch<
+        React.SetStateAction<Keystype[] | undefined>
+    >;
 }
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
@@ -19,9 +24,14 @@ export const AppWrapper: React.FC<{ children: React.ReactNode }> = ({
     children,
 }) => {
     const [keys, setKeys] = useState<Keystype[] | undefined>(undefined);
+    const [filteredKeys, setFilteredKeys] = useState<Keystype[] | undefined>(
+        undefined
+    );
 
     return (
-        <AppContext.Provider value={{ keys, setKeys }}>
+        <AppContext.Provider
+            value={{ keys, setKeys, filteredKeys, setFilteredKeys }}
+        >
             {children}
         </AppContext.Provider>
     );
