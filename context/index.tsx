e3c6448f0dbd194b3b199ed80718c4bbde0c90e6
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-05-07 04:33:56 +0000

    context api for managing keys

---
+++ context/index.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useState, useContext } from "react";
+
+interface Keystype {
+    key: string;
+    key_tag: string;
+    status: string;
+    procurement_date: string;
+    expiry_date: string;
+}
+
+interface AppContextType {
+    keys: Keystype[] | undefined;
+    setKeys: React.Dispatch<React.SetStateAction<Keystype[] | undefined>>;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export const AppWrapper: React.FC<{ children: React.ReactNode }> = ({
+    children,
+}) => {
+    const [keys, setKeys] = useState<Keystype[] | undefined>(undefined);
+
+    return (
+        <AppContext.Provider value={{ keys, setKeys }}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export const useAppContext = () => {
+    const context = useContext(AppContext);
+    if (!context) {
+        throw new Error("useAppContext must be used within an AppWrapper");
+    }
+    return context;
+};
